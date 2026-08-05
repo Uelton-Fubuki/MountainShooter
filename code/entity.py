dc68937d6180479b77f394e0fb228e34457191d2
UFubuki <masterfubuk@hotmail.com>
2026-08-05 00:36:43 -0300

    Fixing import ins Level.py file

--- code/entity.py
+++ code/entity.py
@@ -8,7 +8,7 @@ class Entity(ABC):
     def __init__(self, name: str, position: tuple):
         self.name = name
         # Adicionado o ponto '.' antes de png e corrigido o caminho do asset
-        self.surf = pygame.image.load('./asset/' + name + '.png')
+        self.surf = pygame.image.load('./asset/' + name + '.png').convert_alpha()
         self.rect = self.surf.get_rect(left=position[0], top=position[1])
         self.speed = 0
 
@@ -16,3 +16,6 @@ class Entity(ABC):
     def move(self):
         self.rect.centerx -= 1
         pass
+
+
+    #ENTITY_SPEED[self.name]
